8895f5bdc94cf0d4dfdc5e336dd85e17a4123587
Neeraj Nayan <Neeraj.Nayan@Honeywell.com>
2018-07-04 18:10:27 +0530

    Lesson-27: Redux: Updating Data on the Server

--- app/js/actions/index.js
+++ app/js/actions/index.js
@@ -13,10 +13,13 @@ export const addTodo = (text) => (dispatch) =>
     });
   });
 
-export const toggleTodo = (id) => ({
-  type: 'TOGGLE_TODO',
-  id
-});
+export const toggleTodo = (id) => (dispatch) =>
+  api.toggleTodo(id).then(response => {
+    dispatch({
+      type: 'TOGGLE_TODO_SUCCESS',
+      response: normalize(response, schema.todo)
+    });
+  });
 
 export const setVisibilityFilter = (filter) => ({
   type: 'SET_VISIBILITY_FILTER',
