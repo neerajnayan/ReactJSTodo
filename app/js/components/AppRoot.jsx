2d54b6e3a089145c8b93791c17f0382a938bf059
Neeraj Nayan <Neeraj.Nayan@Honeywell.com>
2018-07-16 18:59:20 +0530

    Lesson-18: Redux: React Todo List Example (Toggling a Todo)

--- app/js/components/AppRoot.jsx
+++ app/js/components/AppRoot.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import store from "../store";
-import { addTodo } from "../actions";
+import { addTodo, toggleTodo } from "../actions";
 
 class AppRoot extends Component {
 	render() {
@@ -21,7 +21,19 @@ class AppRoot extends Component {
 				</button>
 				<ul>
 					{this.props.todos.map(todo => (
-						<li key={todo.id}>{todo.text}</li>
+						<li
+							key={todo.id}
+							onClick={() => {
+								store.dispatch(toggleTodo(todo.id));
+							}}
+							style={{
+								textDecoration: todo.completed
+									? "line-through"
+									: "none"
+							}}
+						>
+							{todo.text}
+						</li>
 					))}
 				</ul>
 			</div>
