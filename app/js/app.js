8f46b90a2afa14d68403e40811297e6ef7fd73c6
Neeraj Nayan <Neeraj.Nayan@Honeywell.com>
2018-07-17 13:42:47 +0530

    Lesson-26: Redux: Passing the Store Down with <Provider> from React Redux

--- app/js/app.js
+++ app/js/app.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 
 import AppRoot from "./components/AppRoot.jsx";
 import reducers from "./reducers";
-import Provider from "./Provider";
 
 ReactDOM.render(
 	<Provider store={createStore(reducers)}>
